496f0eb0ab1e32524e35c0506f508b162314ef72
Nikolai B <nikolai-b@users.noreply.github.com>
2021-06-08 20:02:58 +0100

    Fix quoting arguments
    Passing "" "" means we were passing an empty string as the second
    argument.   is what is needed here

--- cron_entrance_point.sh
+++ cron_entrance_point.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ -z "${BEFORE_EIGHTH}" ] || [ "$(date +'%d')" -lt "8" ]; then
-  /usr/bin/Rscript ./store_directions.R "$1" "$2" "$3" 2>&1 | grep -v "Loading required package: methods"
+  /usr/bin/Rscript ./store_directions.R "$@" 2>&1 | grep -v "Loading required package: methods"
 fi
