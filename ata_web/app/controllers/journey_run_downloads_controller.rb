721f07ad731f696e142fbb1e3938f114dc57e03e
Nikolai B <nikolai-b@users.noreply.github.com>
2024-09-25 14:58:23 +0100

    Add journey downloads

--- ata_web/app/controllers/journey_run_downloads_controller.rb
+++ ata_web/app/controllers/journey_run_downloads_controller.rb
@@ -1,13 +1,33 @@
 class JourneyRunDownloadsController < ApplicationController
-  before_action :set_ltn, only: %i[ new create ]
+  before_action :set_ltn
 
   def new
   end
 
   def create
-    send_data JourneyRun.to_csv(
-      from: Date.iso8601(permitted_params[:from]), to: Date.iso8601(permitted_params[:to]), ltn: @ltn
-    ), filename: "#{@ltn.scheme_name}-#{permitted_params[:from]}-#{permitted_params[:to]}-journey-runs-#{}.csv"
+    from_date =
+      begin
+        Date.iso8601(permitted_params[:from])
+      rescue Date::Error
+        permitted_params[:from]
+      end
+
+    to_date =
+      begin
+        Date.iso8601(permitted_params[:to])
+      rescue Date::Error
+        permitted_params[:to]
+      end
+
+    invalid_input = catch(:invalid_input) do
+      send_data JourneyRun.to_csv(
+        from: from_date, to: to_date,
+        ltn: @ltn,
+        overview_polyline: ActiveModel::Type::Boolean.new.cast(permitted_params[:overview_polyline])
+      ), filename: "#{@ltn.scheme_name}-#{permitted_params[:from]}-#{permitted_params[:to]}-journey-runs-.csv"
+      return
+    end
+    redirect_to new_ltn_journey_run_download_path(@ltn), alert: invalid_input
   end
 
   private
--- ata_web/app/models/journey_run.rb
+++ ata_web/app/models/journey_run.rb
@@ -4,10 +4,10 @@ class JourneyRun < ApplicationRecord
   belongs_to :run
 
   def self.to_csv(from:, to:, ltn:, overview_polyline: false)
-    raise unless to.is_a?(Date)
-    raise unless from.is_a?(Date)
-    raise unless from < to
-    raise if from < to - 1.year
+    throw(:invalid_input, "To: '#{to}' is not a date") unless to.is_a?(Date)
+    throw(:invalid_input, "From: '#{from}' is not a date") unless from.is_a?(Date)
+    throw(:invalid_input, "From: '#{from}' must be before To: '#{to}'") unless from < to
+    throw(:invalid_input, "Can only ask for 1 year of data at a time") if from < to - 1.year
     attributes = %w{journey_id run_id duration duration_in_traffic distance created_at} #customize columns here
     attributes.push("overview_polyline") if overview_polyline
 
