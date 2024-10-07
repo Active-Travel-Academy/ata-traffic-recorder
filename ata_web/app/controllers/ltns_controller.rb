838b80b27a673bcf2235c8e81f89e78fc81e4324
Nikolai B <nikolai-b@users.noreply.github.com>
2024-10-07 17:50:26 +0100

    Show journey type if freq_routed only

--- ata_web/app/controllers/ltns_controller.rb
+++ ata_web/app/controllers/ltns_controller.rb
@@ -9,6 +9,7 @@ class LtnsController < ApplicationController
   # GET /ltns/1
   def show
     @pagy, @journeys = pagy(@ltn.journeys.order(:id), limit: 50)
+    @show_journey_type = @journeys.any? { |jo| jo.type == "frequently_routed" }
   end
 
   # GET /ltns/new
